29925dc8a19bae6debc98e11df3194ce29b6f5d6
Saimir Bala <bala@ai.wu.ac.at>
2018-12-26 01:27:44 +0100

    Fix *tree* bug.

--- ExtractMovieFromAsana/src/main/java/at/ac/wu/asana/viz/test/CreateVizAsana.java
+++ ExtractMovieFromAsana/src/main/java/at/ac/wu/asana/viz/test/CreateVizAsana.java
@@ -2,14 +2,20 @@ package at.ac.wu.asana.viz.test;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.graphstream.graph.Edge;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
-import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.ui.spriteManager.Sprite;
 import org.graphstream.ui.spriteManager.SpriteManager;
+import org.graphstream.ui.view.Viewer;
 import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import at.ac.wu.asana.db.utils.DatabaseConnector;
@@ -21,36 +27,42 @@ public class CreateVizAsana {
 	public static void main(String[] args) {
 		System.setProperty("org.graphstream.ui.renderer", "org.graphstream.ui.j2dviewer.J2DGraphRenderer");
 
-		Graph graph = new SingleGraph("Asana graph");
+		Graph graph = new MultiGraph("Asana graph");
 		
-		graph.addAttribute("ui.stylesheet", "url('file:///home/saimir/asana/ExtractMovieFromAsana/src/resources/styleSheetAsana.css')");
+		graph.addAttribute("ui.stylesheet", "url('file:///home/saimir/asana/ExtractMovieFromAsana/src/resources/styleSheetAsanaMinimal.css')");
 		graph.setStrict(false);
 		graph.setAutoCreate(true);
 		graph.addAttribute("ui.quality");
 		graph.addAttribute("ui.antialias");
 
-		Node master = graph.addNode("11347525454570");
+		Viewer viewer = graph.display();
+		Node master = graph.addNode("864733919245"); // Springest center
+		
+		viewer.disableAutoLayout();
 
-		master.addAttribute("ui.class", "circleClass");
-		master.addAttribute("ui.label", "☺ Organisations Roles");
-
-		graph.display();
-
+		master.addAttribute("ui.class", "master");
+		master.addAttribute("ui.label", "Springest");
+		
+		viewer.enableAutoLayout();
+		
 		addNodesStepByStep(graph, master);
 		
 		SpriteManager sManager = new SpriteManager(graph);
 		Sprite s1 = sManager.addSprite("S1");
-		s1.setPosition(1, 1, 0);
+		s1.setPosition(0, 0, 0);
+		s1.attachToNode(master.getId());
 		s1.setAttribute("ui.label", "End of movie");		
 	}
 
 	private static void addNodesStepByStep(Graph graph, Node master) {
 
 		SessionFactory sf = DatabaseConnector.getSessionFactory("asana_manual2");
-
 		org.hibernate.Session session = sf.openSession();
-		Query queryAllDates = session.createSQLQuery("SELECT DISTINCT date FROM `TABLE 3` ORDER BY date ");
-		Query queryEvents = session.createSQLQuery("SELECT * FROM `TABLE 3` WHERE date =:date ORDER by timestamp ASC");
+		
+		addAllCircles(session, graph, master);
+		
+		Query queryAllDates = session.createSQLQuery("SELECT DISTINCT date FROM `TABLE 2` ORDER BY date");
+		Query queryEvents = session.createSQLQuery("SELECT * FROM `TABLE 2` WHERE date =:date ORDER by timestamp ASC");
 
 		List<String> result = (List<String>) queryAllDates.list();		
 		System.out.println("Retrieved "+result.size()+ " days of history.");
@@ -70,13 +82,60 @@ public class CreateVizAsana {
 				//				System.out.println("i="+i+++" "+sdc.getTaskId()+" "+sdc.getTypeOfChangeDescription()+" "+
 				//				sdc.getCreatedAt()+" prjId="+sdc.getProjectId());
 			}
-			List<Node> nodesAdded = addToGraph(graph, changeEvents);
+//			List<Node> nodesAdded = 
+					addToGraph(graph, changeEvents);
+					connectSameCircleNodes(graph);
 			sleep(1000);
-			for (Node node : nodesAdded) {
-				node.addAttribute("ui.style", "fill-color: rgb(155,0,0);");
-			}
+//			for (Node node : nodesAdded) {
+//				node.addAttribute("ui.style", "fill-color: rgb(155,0,0);");
+//			}
 		}
 		sf.close();
+	}
+
+	private static void connectSameCircleNodes(Graph graph) {
+		Collection<Node> nodes = graph.getNodeSet();
+		Map<Object, List<Node>> circleNodeMap = new java.util.HashMap<Object, List<Node>>();		
+		for (Node n : nodes) {
+			if(circleNodeMap.containsKey(n.getAttribute("circle"))) {
+				circleNodeMap.get(n.getAttribute("circle")).add(n);
+			}
+			else {
+				List<Node> l = new ArrayList<Node>();
+				l.add(n);
+				circleNodeMap.put(n.getAttribute("circle"), l);
+			}
+		}
+		Set<Object> keys = circleNodeMap.keySet();
+		for (Object k : keys) {
+			List<Node> list = circleNodeMap.get(k);
+			for(int i=0; i<list.size()-1; i++) {
+				for(int j=i+1; j<list.size(); j++) {
+					Node n1 = list.get(i);
+					Node n2 = list.get(j);
+					Edge e = graph.addEdge(n1.getId()+n2.getId(), n1.getId(), n2.getId());
+					e.addAttribute("ui.class", "sameCircle");
+				}
+			}
+		}
+		
+	}
+
+	private static void addAllCircles(Session session, Graph graph, Node master) {
+		// TODO Auto-generated method stub SELECT DISTINCT projectName FROM `Springest` WHERE 1 
+		Query queryProjects = session.createSQLQuery("SELECT DISTINCT projectId, projectName FROM `Springest` WHERE 1");
+		List projects = queryProjects.list();
+		
+		for (Object object : projects) {
+			Object[] row = (Object[]) object;
+			String[] str = toStrObjArray(row);
+			
+			Edge e = graph.addEdge(master.getId()+str[0], master.getId(), str[0]);
+			Node n = e.getTargetNode();
+			n.addAttribute("ui.class", "circleClass");
+			n.addAttribute("ui.label", str[1]);
+		}
+		
 	}
 
 	private static String[] toStrObjArray(Object[] row) {
@@ -105,6 +164,7 @@ public class CreateVizAsana {
 				}
 
 				n = graph.getNode(e.getTaskId());
+				n.addAttribute("circle", e.getProjectId());
 				if(e.getTaskName()!=null)
 					n.addAttribute("ui.label", e.getTaskName());
 
@@ -114,6 +174,8 @@ public class CreateVizAsana {
 
 				if(StructuralDataChange.isCircle(e.getTaskName())) {
 					n.addAttribute("ui.class", "circleClass");
+					if(e.getIsSubtask())
+						n.addAttribute("ui.class", "subcircle");
 				}
 			}
 			switch(e.getTypeOfChange()) {
@@ -121,34 +183,35 @@ public class CreateVizAsana {
 			case AsanaActions.ADD_TO_CIRCLE:
 			case AsanaActions.ASSIGN_TO_ACTOR:
 			case AsanaActions.CREATE_ROLE:
-				n.addAttribute("ui.class", "fill-color: green;");
+				n.addAttribute("ui.class", "added");
 				break;
 			case AsanaActions.REVIVE_OR_MARK_INCOMPLETE:
-				if(e.getIsSubtask()) {
-					org.graphstream.graph.Edge ed = 
-							graph.addEdge(e.getParentTaskId()+e.getTaskId(), e.getParentTaskId(), e.getTaskId());
-					nodesAdded.add(ed.getNode0());
-					nodesAdded.add(ed.getNode1());
-				}
-				else {
-					org.graphstream.graph.Edge ed =
-							graph.addEdge(e.getProjectId()+e.getTaskId(), e.getProjectId(), e.getTaskId());
-					nodesAdded.add(ed.getNode0());
-					nodesAdded.add(ed.getNode1());
-				}
-				n.addAttribute("ui.class", "role");
+//				if(e.getIsSubtask()) {
+//					org.graphstream.graph.Edge ed = 
+//							graph.addEdge(e.getParentTaskId()+e.getTaskId(), e.getParentTaskId(), e.getTaskId());
+//					nodesAdded.add(ed.getNode0());
+//					nodesAdded.add(ed.getNode1());
+//				}
+//				else {
+//					org.graphstream.graph.Edge ed =
+//							graph.addEdge(e.getProjectId()+e.getTaskId(), e.getProjectId(), e.getTaskId());
+//					nodesAdded.add(ed.getNode0());
+//					nodesAdded.add(ed.getNode1());
+//				}
+//				n.addAttribute("ui.class", "role");
 				break;
 			case AsanaActions.COMPLETE_ROLE:
 			case AsanaActions.DETELE_OR_MARK_COMPLETE:
 			case AsanaActions.REMOVE_FROM_CIRCLE:
-				n.addAttribute("ui.class", "deleted");
 //				sleep(1000);
-//				graph.removeNode(n);
+				Node n2 = graph.removeNode(n);
+				n.addAttribute("ui.class", "unemphasize");
+				sleep(100);
 				break;
 			default:
 				break;
 			}
-//			sleep(500);
+			sleep(500);
 		}
 		return nodesAdded;
 	}
