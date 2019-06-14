ac63b27172684f3095d697b520e35b9bd4fdad71
Saimir Bala <s41m1r@users.noreply.github.com>
2019-06-14 11:59:17 +0200

    Delete extractChangesOfAllProjects.sh

--- ExtractMovieFromAsana/extractChangesOfAllProjects.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while read p; do
-  echo "$p"
-  java -cp extract-asana-events-jar-with-dependencies.jar at.ac.wu.asana.csv.ExtractStructuralDataChanges -csv "/home/saimir/ownCloud/PhD/Collaborations/Waldemar/API/$p.csv" -pat "0/7506773dd733d4efc682cd23d5949372" -ws "Springest" -p "$p"
-done < allSmileys.txt
