3fab9b6229977cefac7d735be61c6bc082fc714d
Saimir Bala <bala@ai.wu.ac.at>
2019-08-13 15:40:11 +0200

    Fixed extraction of task notes

--- ExtractMovieFromAsana/src/main/java/at/ac/wu/asana/tryout/TryTaskType.java
+++ ExtractMovieFromAsana/src/main/java/at/ac/wu/asana/tryout/TryTaskType.java
@@ -2,14 +2,9 @@ package at.ac.wu.asana.tryout;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 import com.asana.Client;
-import com.asana.models.CustomField;
-import com.asana.models.Project;
-import com.asana.models.Resource;
-import com.asana.models.Section;
 import com.asana.models.Task;
 
 public class TryTaskType {
@@ -18,8 +13,9 @@ public class TryTaskType {
 		Client client = Client.accessToken(pat);
 		client.options.put("page_size", 100);
 		client.options.put("max_retries", 100);
+		client.options.put("archived", true);
 		
-		List<Task> tasksIt = client.tasks.findByProject("61971534223290").query("resource_subtype", "default_task").
+		List<Task> tasksIt = client.tasks.findByProject("12530878841888").query("resource_subtype", "default_task").
 				option("fields",
 						Arrays.asList(
 								"created_at", "name", "completed",
@@ -31,7 +27,7 @@ public class TryTaskType {
 		
 		for (Task task : tasksIt) {
 			System.out.println(task.resourceSubtype+" - "+task.resourceType+
-					" - "+task.name+" - "+task.customFields);
+					" - "+task.name+" - "+task.customFields+" ");
 		}
 	}
 }
